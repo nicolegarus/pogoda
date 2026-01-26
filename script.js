6f7518928b769e25dde8e3ef6f1d4bc865c1dff1
nicolegarus <nicolegarus0@gmail.com>
2026-01-26 16:49:35 +0000

    Added the search functionality using the API

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "8051d8dad8aaf85f7ca939803db9f063";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Warsaw");
